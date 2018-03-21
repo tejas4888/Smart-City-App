db056bd406d4673518d925784ce6c4699e63e60a
Mithil Dani <danimithil@gmail.com>
2018-03-21 17:24:00 +0530

    Polls buttons working. Data processing left

--- app/src/main/java/com/example/tejas/smartcityapp/RecyclerAdapter/PollsRecyclerAdapter.java
+++ app/src/main/java/com/example/tejas/smartcityapp/RecyclerAdapter/PollsRecyclerAdapter.java
@@ -1,15 +1,21 @@
 package com.example.tejas.smartcityapp.RecyclerAdapter;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.tejas.smartcityapp.Items.PollsItem;
 import com.example.tejas.smartcityapp.R;
+import com.example.tejas.smartcityapp.UserDetailsActivity;
 
 import java.util.ArrayList;
 
@@ -35,7 +41,7 @@ public class PollsRecyclerAdapter extends RecyclerView.Adapter<PollsRecyclerAdap
     }
 
     @Override
-    public void onBindViewHolder(PollsRecyclerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(final PollsRecyclerAdapter.ViewHolder holder, final int position) {
 
         holder.question_textview.setText(arrayList.get(position).question);
 
@@ -46,6 +52,45 @@ public class PollsRecyclerAdapter extends RecyclerView.Adapter<PollsRecyclerAdap
                 //do intents etc.
             }
         });*/
+
+        holder.radioButtonYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holder.radioButtonNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holder.radioButtonCantsay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holder.submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.radioButtonYes.isChecked()){
+                    submitAnswer(arrayList.get(position).id, 1, position, holder);
+                }
+                else if (holder.radioButtonNo.isChecked()){
+                    submitAnswer(arrayList.get(position).id, 2, position, holder);
+                }
+                else if (holder.radioButtonCantsay.isChecked()){
+                    submitAnswer(arrayList.get(position).id, 3, position, holder);
+                }
+                else {
+
+                }
+            }
+        });
     }
 
     @Override
@@ -57,13 +102,70 @@ public class PollsRecyclerAdapter extends RecyclerView.Adapter<PollsRecyclerAdap
     {
         TextView question_textview;
         CardView cardView;
+        RadioButton radioButtonYes, radioButtonNo, radioButtonCantsay;
+        Button submitButton;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             question_textview=itemView.findViewById(R.id.polls_question);
             cardView=itemView.findViewById(R.id.polls_item_cardview);
+            radioButtonYes = itemView.findViewById(R.id.polls_yes);
+            radioButtonNo = itemView.findViewById(R.id.polls_no);
+            radioButtonCantsay = itemView.findViewById(R.id.polls_cantsay);
+            submitButton = itemView.findViewById(R.id.polls_submit_button);
         }
+
+
     }
 
+    private void submitAnswer(final String id, int answer, final int position, final ViewHolder holder){
+        class SubmitAnswer extends AsyncTask<Void,Void,String>{
+
+            ProgressDialog progressDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(PollsRecyclerAdapter.this.context,"Submitting Answer","Please Wait...",false,
+                        false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                progressDialog.dismiss();
+
+                // To remove other options from the card
+                if (holder.radioButtonYes.isChecked()){
+                    holder.radioButtonNo.setVisibility(View.GONE);
+                    holder.radioButtonCantsay.setVisibility(View.GONE);
+                    holder.submitButton.setVisibility(View.GONE);
+                }
+                else if (holder.radioButtonNo.isChecked()){
+                    holder.radioButtonYes.setVisibility(View.GONE);
+                    holder.radioButtonCantsay.setVisibility(View.GONE);
+                    holder.submitButton.setVisibility(View.GONE);
+                }
+                else if (holder.radioButtonCantsay.isChecked()){
+                    holder.radioButtonYes.setVisibility(View.GONE);
+                    holder.radioButtonNo.setVisibility(View.GONE);
+                    holder.submitButton.setVisibility(View.GONE);
+                }
+                else {
+                    // To remove that card
+                    PollsRecyclerAdapter.this.arrayList.remove(position);
+                    PollsRecyclerAdapter.this.notifyItemRemoved(position);
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+
+                return null;
+            }
+        }
+        SubmitAnswer a = new SubmitAnswer();
+        a.execute();
+    }
 }
