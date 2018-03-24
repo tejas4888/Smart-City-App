2520c44638b495fa5a3855548ea461159e470ad3
Tejas Chheda <tejas4888@gmail.com>
2018-03-24 09:27:24 +0530

    Just added recycler layout and fragment for alert. Testing merge

---
+++ app/src/main/java/com/example/tejas/smartcityapp/Fragments/AlertsFragment.java
@@ -0,0 +1,32 @@
+package com.example.tejas.smartcityapp.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tejas.smartcityapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AlertsFragment extends Fragment {
+
+
+    public AlertsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_alerts, container, false);
+
+        return view;
+    }
+
+}
