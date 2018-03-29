7d95569a70ef592cd6920cb7effd5ed16f8e1227
Tejas Chheda <tejas4888@gmail.com>
2018-03-29 10:40:07 +0530

    Added Carousel and description

--- app/src/main/java/com/example/tejas/smartcityapp/Fragments/MainFragment.java
+++ app/src/main/java/com/example/tejas/smartcityapp/Fragments/MainFragment.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.tejas.smartcityapp.R;
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ViewListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +26,9 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    CarouselView carouselView;
+    TextView city_description_textview;
+
     public static MainFragment newInstance(){
         MainFragment mainFragment = new MainFragment();
         return mainFragment;
@@ -28,7 +38,41 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        carouselView=(CarouselView)view.findViewById(R.id.fragment_main_carousel);
+        carouselView.setPageCount(3);
+        carouselView.setViewListener(carouselViewListener);
+
+        city_description_textview=(TextView)view.findViewById(R.id.fragment_main_city_description);
+        city_description_textview.setText(R.string.city_description);
+
+        return view;
     }
+
+    ViewListener carouselViewListener=new ViewListener() {
+        @Override
+        public View setViewForPosition(int position) {
+
+            LinearLayout carousel_linear_container=(LinearLayout) getActivity().findViewById(R.id.carousel_item_linear_container);
+
+            View carouselItemView=getActivity().getLayoutInflater().inflate(R.layout.fragment_main_carousel_item,carousel_linear_container,false);
+            KenBurnsView carousel_image=(KenBurnsView) carouselItemView.findViewById(R.id.carousel_item_kenburns);
+
+            if (position==0)
+            {
+                carousel_image.setImageResource(R.drawable.home_carousel_1);
+            }
+            else if (position==1)
+            {
+                carousel_image.setImageResource(R.drawable.home_carousel_2);
+            }else
+            {
+                carousel_image.setImageResource(R.drawable.home_carousel_3);
+            }
+
+            return carouselItemView;
+        }
+    };
 
 }
